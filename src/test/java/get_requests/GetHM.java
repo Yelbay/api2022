15910d1b6f492b29f341f18dbbe1a911ba280842
“Yelbaygit config --global user.email cnntylby@gmail.comgit config --global credential.username Yelbaygit config --global credential.password Cennet1983 <cnntylby@gmail.comgit config --global user.name Yelbay>
2022-06-29 22:32:54 -0400

    last commit

---
+++ src/test/java/get_requests/GetHM.java
@@ -0,0 +1,25 @@
+package get_requests;
+
+public class GetHM {
+
+     /*
+        Given
+            https://restful-booker.herokuapp.com/booking/2022
+        When
+            User send a GET Request to the url
+        Then
+            HTTP Status code should be 404
+        And
+            Status Line should be HTTP/1.1 404 Not Found
+        And
+            Response body contains "Not Found"
+        And
+            Response body does not contain "API is fun"
+        And
+            Connection is "keep-alive"
+        And
+           Server is not "RedKit"
+     */
+
+
+}
